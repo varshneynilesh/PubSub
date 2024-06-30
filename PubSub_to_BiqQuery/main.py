1bd85e84bb1057c7210d18c344fed19313d78614
varshneynilesh <84116474+varshneynilesh@users.noreply.github.com>
2024-06-30 16:19:39 -0700

    initial

--- PubSub_to_BiqQuery/main.py
+++ PubSub_to_BiqQuery/main.py
@@ -1,1 +1,17 @@
-Hello
+import json
+import csv
+from google.cloud import pubsub
+
+project_name = '<PROJECT-NAME>'
+topic_name = 'purchases'
+file = 'data/customers_data.csv'
+
+publisher = pubsub.PublisherClient()
+topic_path = publisher.topic_path(project_name, topic_name)
+
+with open(file) as fh:
+    rd = csv.DictReader(fh, delimiter=',')
+    for row in rd:
+        data = json.dumps(dict(row))
+        publisher.publish(topic_path, data=data.encode('utf-8'))
+
